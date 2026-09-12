183769e158137fc358fd4183e6c76dbd7c277d39
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-12 17:15:34 -0300

    test(application): cover SettingsStoreError formatting

--- crates/application/src/errors/settings_store_error.rs
+++ crates/application/src/errors/settings_store_error.rs
@@ -30,3 +30,39 @@ impl fmt::Display for SettingsStoreError {
 }
 
 impl Error for SettingsStoreError {}
+
+#[cfg(test)]
+mod settings_store_error_tests {
+    use super::*;
+
+    #[test]
+    fn unreadable_variant_constructs_and_displays() {
+        let error = SettingsStoreError::Unreadable("disk".into());
+
+        assert_eq!(error.to_string(), "the settings could not be read: disk");
+    }
+
+    #[test]
+    fn unwritable_variant_constructs_and_displays() {
+        let error = SettingsStoreError::Unwritable("disk".into());
+
+        assert_eq!(error.to_string(), "the settings could not be saved: disk");
+    }
+
+    #[test]
+    fn malformed_variant_constructs_and_displays() {
+        let error = SettingsStoreError::Malformed("syntax".into());
+
+        assert_eq!(
+            error.to_string(),
+            "the stored settings are malformed: syntax"
+        );
+    }
+
+    #[test]
+    fn settings_store_error_implements_std_error() {
+        let error = SettingsStoreError::Unreadable("disk".into());
+
+        let _: &dyn std::error::Error = &error;
+    }
+}
