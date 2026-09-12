6d08e0ebf8bcc3793737b94dc28dcdfdd6630c8f
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-12 17:09:25 -0300

    feat(application): add settings load inbound port

--- crates/application/src/ports/inbound/mod.rs
+++ crates/application/src/ports/inbound/mod.rs
@@ -1,6 +1,7 @@
 pub mod inspect_model_port;
 pub mod install_model_port;
 pub mod list_installed_models_port;
+pub mod load_settings_port;
 pub mod prune_library_port;
 pub mod remove_model_port;
 pub mod search_models_port;
@@ -9,6 +10,7 @@ pub mod verify_model_port;
 pub use inspect_model_port::InspectModelPort;
 pub use install_model_port::InstallModelPort;
 pub use list_installed_models_port::ListInstalledModelsPort;
+pub use load_settings_port::LoadSettingsPort;
 pub use prune_library_port::PruneLibraryPort;
 pub use remove_model_port::RemoveModelPort;
 pub use search_models_port::SearchModelsPort;
